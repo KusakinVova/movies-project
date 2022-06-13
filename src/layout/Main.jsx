7f7f9c1b97207cd418e94e946f7daed20837ea54
Vladimir <kusakinvova@gmail.com>
2022-06-14 01:03:20 +0300

    feat: connect component Search

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,23 +1,31 @@
 import React from "react";
 import { Movies } from "../components/Movies";
 import { Preloader } from "../components/Preloader";
+import { Search } from "../components/Search";
 
 class Main extends React.Component{
 
   state = {
-    movies: []
+    movies: [],
+    search: 'matrix',
+    typemovie: '',
   }
 
   componentDidMount(){
-    fetch('http://www.omdbapi.com/?apikey=a1171f9e&s=matrix')
+    this.searchMovies(this.state.search, this.state.typemovie);
+  }
+
+  searchMovies = (str, typemovie) => {
+    fetch(`http://www.omdbapi.com/?apikey=a1171f9e&s=${str}${ typemovie !== '' ? `&type=${typemovie}` : ''}`)
       .then(response => response.json())
       .then(data => {this.setState({movies: data.Search}); console.log(data.Search);  });
   }
 
   render(){
-    const {movies} = this.state;
+    const {movies, search, typemovie} = this.state;
     return (
       <main className="content container">
+        <Search searchMovies={this.searchMovies} search={search} typemovie={typemovie} />
         {
           movies.length ?
             <Movies movies={movies} />
