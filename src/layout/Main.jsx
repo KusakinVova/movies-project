b250e3b5d94cce2efa2d22bce0a7234552b90421
Vladimir <kusakinvova@gmail.com>
2022-06-14 18:07:53 +0300

    feat: add const api_key

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,6 +3,8 @@ import { Movies } from "../components/Movies";
 import { Preloader } from "../components/Preloader";
 import { Search } from "../components/Search";
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 class Main extends React.Component{
 
   state = {
@@ -18,7 +20,7 @@ class Main extends React.Component{
 
   searchMovies = (str, typemovie) => {
     this.setState({loading: true});
-    fetch(`http://www.omdbapi.com/?apikey=a1171f9e&s=${str}${ typemovie !== '' ? `&type=${typemovie}` : ''}`)
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${ typemovie !== '' ? `&type=${typemovie}` : ''}`)
       .then(response => response.json())
       .then(data => {
         this.setState({movies: data.Search, loading: false});
